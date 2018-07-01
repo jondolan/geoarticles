4c693e4f55b71b3b1cd2eb93485356fc5077aeb9
Jon Dolan <jondolan33@gmail.com>
2018-07-01 16:39:58 -0400

    Initial flask application frontend upload

---
+++ static/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    // alert("hey there!");
+    // $("#search-box").on("change paste keyup", new_query);
+    $("#search-button").on("click", new_query);
+});
+
+function new_query(event) {
+    // console.log($("#search-box").val());
+    
+    $.post("/search", { query: $("#search-box").val() }, function(data) {
+        console.log(data);
+        var res = ""
+        for (var key in data) {
+            // console.log(data[key].url);
+            res += "<a href='" + data[key].url + "'>" + data[key].title + "</a><br />";
+        }
+        $("#result").html(res);
+    });
+}
--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,32 @@
 import os
-
 #print(os.environ['MONGODB_CONNECTION_STRING'])
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
 
-from flask import Flask
 application = Flask(__name__)
+client = MongoClient(os.environ['MONGODB_CONNECTION_STRING'])
+# client = MongoClient('localhost', 27017)
+articles_db = client.articles
+syriahr = articles_db.syriahr
 
 @application.route("/")
-def hello():
-    return "Hello World!"
+def render():
+    return render_template('homepage.html')
+
+@application.route('/search', methods = ['POST'])
+def search():
+    global syriahr
+    if request.method == 'POST':
+        value = request.form['query']
+        results = syriahr.find({"$text": {"$search": value}})
+        i = 0
+        rtn = {}
+        for result in results:
+            rtn[str(i)] = result
+            i+=1
+        # print(rtn)
+        # print(jsonify(rtn))
+        return jsonify(rtn)
 
 if __name__ == "__main__":
     application.run()
